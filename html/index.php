67e4010adc75c397f49a1bb04c30375588f0cb80
night-lin <592615616@qq.com>
2016-05-26 18:06:21 +0800

    登录页和首页

---
+++ html/index.php
@@ -0,0 +1,44 @@
+<!Doctype html>
+	<head>
+		<meta charset="utf-8">
+		<title>Shopping-site</title>
+		<link href="../css/index.css"  type="text/css" rel="stylesheet"/>
+		<script src="../js/jquery.min.js"></script>
+		
+	</head>
+	<body>
+		<div id="nav">
+			<div id="nav-bar">
+			 	<img src="../images/logo-nav.png">
+			 	<ul class="nav-ul">
+			 		<li class="li-now"><a class="nav-a" href="#">首页</a></li>
+			 		<li><a class="nav-a" href="#">账户设置</a></li>
+			 		<li><a class="nav-a" href="#">订单情况</a></li>
+			 	</ul>
+			 	<div id="search-area">
+			 		
+			 	      <form action="">
+			 	      	<input placeholder="请输入想要搜索的商品" type="text" name="goods-name">
+			 	      	<button class="btn-default btn-search">搜索</button>
+				 	  </form>	
+			 		
+			 	</div>	
+			</div>
+		</div>
+		<div id="main-content">
+			<div class="banner">
+					<div class="b-img">
+
+					  	<a href="javascript:void(0)" style="background:url(../images/banner1.jpg) center no-repeat;"></a>
+					  	<a href="javascript:void(0)" style="background:url(../images/banner2.jpg) center no-repeat;"></a>
+					  	<a href="javascript:void(0)" style="background:url(../images/banner1.jpg) center no-repeat;"></a>
+					 </div>
+					 <div class="b-list-bg"></div>
+					 <div class="b-list"></div>
+					 <a class="bar-left" href="javascript:void(0)"><em></em></a>
+					 <a class="bar-right" href="javascript:void(0)"><em></em></a>
+			</div>	
+		</div>
+	</body>
+	<script src="../js/banner.js"></script>
+</html>
